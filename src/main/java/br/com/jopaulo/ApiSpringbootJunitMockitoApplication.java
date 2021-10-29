6af8e600bea4e26f2cf6ad9097d03a0e12145c66
João Paulo <jp_cbc@hotmail.com>
2021-10-29 09:05:38 -0300

    Iniciando o projeto e classe User

---
+++ src/main/java/br/com/jopaulo/ApiSpringbootJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.jopaulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootJunitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jopaulo/ApiSpringbootJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jopaulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootJunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
